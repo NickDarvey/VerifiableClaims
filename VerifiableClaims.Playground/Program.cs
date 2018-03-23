c1d16d363ac2f1e1afa5ac032d15df8e06242e90
Nick Darvey <nickdarvey@outlook.com.au>
2018-03-24 00:09:53 +1100

    Adds playground for testing library out in C#.

---
+++ VerifiableClaims.Playground/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Immutable;
+using NickDarvey.VerifiableClaims.Schema;
+
+namespace NickDarvey.VerifiableClaims.Playround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var profileId = ProfileId.NewProfileId(new Uri("urn:my:id"));
+
+            Console.WriteLine(profileId);
+        }
+    }
+}
