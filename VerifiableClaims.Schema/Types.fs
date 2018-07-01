aa471ef0b3dca80d9aa0714474511a346c4932fc
Nick Darvey <nickdarvey@outlook.com.au>
2018-07-01 17:09:30 +1000

    Moves types into root namespace

--- VerifiableClaims.Schema/Types.fs
+++ VerifiableClaims.Schema/Types.fs
@@ -1,48 +1,50 @@
-namespace NickDarvey.VerifiableClaims.Schema
+namespace NickDarvey.VerifiableClaims
 
-open System
+[<AutoOpen>]
+module Types =
+    open System
 
-type set<'T when 'T : comparison> = System.Collections.Immutable.IImmutableSet<'T>
+    type set<'T when 'T : comparison> = System.Collections.Immutable.IImmutableSet<'T>
 
-type ProofType = SignatureType of string
-type Proof = {
-    Type: ProofType
-}
+    type ProofType = SignatureType of string
+    type Proof = {
+        Type: ProofType
+    }
 
-type ProfileId = ProfileId of Uri
-type ProfileType = ProfileType of string
-type Profile = { 
-    Id: ProfileId
-    Type: ProfileType set
-    Proof: Proof option
-}
+    type ProfileId = ProfileId of Uri
+    type ProfileType = ProfileType of string
+    type Profile = { 
+        Id: ProfileId
+        Type: ProfileType set
+        Proof: Proof option
+    }
 
-type CredentialStatusId = RevocationId of Uri
-type CredentialStatusType = RevocationType of string
-type CredentialStatus = {
-    Id: CredentialStatusId
-    Type: CredentialStatusType set
-}
+    type CredentialStatusId = RevocationId of Uri
+    type CredentialStatusType = RevocationType of string
+    type CredentialStatus = {
+        Id: CredentialStatusId
+        Type: CredentialStatusType set
+    }
 
-type CredentialId = CredentialId of Uri
-type CredentialType = CredentialType of string
-type Claim = { Id: ProfileId }
-type Credential = {
-    Id: CredentialId
-    Type: CredentialType set
-    Issuer: ProfileId
-    Issued: DateTimeOffset
-    Expires: DateTimeOffset option
-    Claim: Claim
-    CredentialStatus: CredentialStatus option
-    Proof: Proof option
-}
+    type CredentialId = CredentialId of Uri
+    type CredentialType = CredentialType of string
+    type Claim = { Id: ProfileId }
+    type Credential = {
+        Id: CredentialId
+        Type: CredentialType set
+        Issuer: ProfileId
+        Issued: DateTimeOffset
+        Expires: DateTimeOffset option
+        Claim: Claim
+        CredentialStatus: CredentialStatus option
+        Proof: Proof option
+    }
 
-type PresentationId = PresentationId of Uri
-type PresentationType = PresentationType of string
-type Presentation = {
-    Id: PresentationId
-    Type: PresentationType set
-    VerifiableCredential: Credential list
-    Proof: Proof list
-}
+    type PresentationId = PresentationId of Uri
+    type PresentationType = PresentationType of string
+    type Presentation = {
+        Id: PresentationId
+        Type: PresentationType set
+        VerifiableCredential: Credential list
+        Proof: Proof list
+    }
